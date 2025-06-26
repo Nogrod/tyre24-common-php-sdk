0598edf1e450976ca5835771d40db4f38d676553
Nogrod <strikeomania@gmail.com>
2025-06-26 17:09:59 +0200

    open api gen 7.14

--- src/FormDataProcessor.php
+++ src/FormDataProcessor.php
@@ -1,10 +1,10 @@
 <?php
 
 /**
- * Tax
- *
+ * FormDataProcessor
  * PHP version 8.1
  *
+ * @category Class
  * @package  Tyre24\Common
  * @author   OpenAPI Generator team
  * @link     https://openapi-generator.tech
@@ -27,417 +27,212 @@
  * Do not edit the class manually.
  */
 
-namespace Tyre24\Common\Model;
+namespace Tyre24\Common;
 
 use ArrayAccess;
-use JsonSerializable;
-use InvalidArgumentException;
-use ReturnTypeWillChange;
-use Tyre24\Common\ObjectSerializer;
+use DateTime;
+use GuzzleHttp\Psr7\Utils;
+use Psr\Http\Message\StreamInterface;
+use SplFileObject;
+use Tyre24\Common\Model\ModelInterface;
 
-/**
- * Tax Class Doc Comment
- *
- * @package  Tyre24\Common
- * @author   OpenAPI Generator team
- * @link     https://openapi-generator.tech
- * @implements ArrayAccess<string, mixed>
- */
-class Tax implements ModelInterface, ArrayAccess, JsonSerializable
+class FormDataProcessor
 {
-    public const DISCRIMINATOR = null;
-
     /**
-      * The original name of the model.
-      *
-      * @var string
-      */
-    protected static string $openAPIModelName = 'Tax';
-
-    /**
-      * Array of property to type mappings. Used for (de)serialization
-      *
-      * @var array<string, string>
-      */
-    protected static array $openAPITypes = [
-        'tax_number' => 'string',
-        'sales_tax_identification_number' => 'string'
-    ];
-
-    /**
-      * Array of property to format mappings. Used for (de)serialization
-      *
-      * @var array<string, string|null>
-      */
-    protected static array $openAPIFormats = [
-        'tax_number' => null,
-        'sales_tax_identification_number' => null
-    ];
-
-    /**
-      * Array of nullable properties. Used for (de)serialization
-      *
-      * @var array<string, bool>
-      */
-    protected static array $openAPINullables = [
-        'tax_number' => false,
-        'sales_tax_identification_number' => false
-    ];
-
-    /**
-      * If a nullable field gets set to null, insert it here
-      *
-      * @var array<string, bool>
-      */
-    protected array $openAPINullablesSetToNull = [];
-
-    /**
-     * Array of property to type mappings. Used for (de)serialization
-     *
-     * @return array<string, string>
+     * Tags whether payload passed to ::prepare() contains one or more
+     * SplFileObject or stream values.
      */
-    public static function openAPITypes(): array
+    public bool $has_file = false;
+
+    /**
+     * Take value and turn it into an array suitable for inclusion in
+     * the http body (form parameter). If it's a string, pass through unchanged
+     * If it's a datetime object, format it in ISO8601
+     *
+     * @param array<string|bool|array|DateTime|ArrayAccess|SplFileObject> $values the value of the form parameter
+     *
+     * @return array [key => value] of formdata
+     */
+    public function prepare(array $values): array
     {
-        return self::$openAPITypes;
-    }
+        $this->has_file = false;
+        $result = [];
 
-    /**
-     * Array of property to format mappings. Used for (de)serialization
-     *
-     * @return array<string, string>
-     */
-    public static function openAPIFormats(): array
-    {
-        return self::$openAPIFormats;
-    }
+        foreach ($values as $k => $v) {
+            if ($v === null) {
+                continue;
+            }
 
-    /**
-     * Array of nullable properties
-     *
-     * @return array<string, bool>
-     */
-    protected static function openAPINullables(): array
-    {
-        return self::$openAPINullables;
-    }
-
-    /**
-     * Array of nullable field names deliberately set to null
-     *
-     * @return array<string, bool>
-     */
-    private function getOpenAPINullablesSetToNull(): array
-    {
-        return $this->openAPINullablesSetToNull;
-    }
-
-    /**
-     * Setter - Array of nullable field names deliberately set to null
-     *
-     * @param array<string, bool> $openAPINullablesSetToNull
-     */
-    private function setOpenAPINullablesSetToNull(array $openAPINullablesSetToNull): void
-    {
-        $this->openAPINullablesSetToNull = $openAPINullablesSetToNull;
-    }
-
-    /**
-     * Checks if a property is nullable
-     *
-     * @param string $property
-     * @return bool
-     */
-    public static function isNullable(string $property): bool
-    {
-        return self::openAPINullables()[$property] ?? false;
-    }
-
-    /**
-     * Checks if a nullable property is set to null.
-     *
-     * @param string $property
-     * @return bool
-     */
-    public function isNullableSetToNull(string $property): bool
-    {
-        return in_array($property, $this->getOpenAPINullablesSetToNull(), true);
-    }
-
-    /**
-     * Array of attributes where the key is the local name,
-     * and the value is the original name
-     *
-     * @var array<string, string>
-     */
-    protected static array $attributeMap = [
-        'tax_number' => 'tax_number',
-        'sales_tax_identification_number' => 'sales_tax_identification_number'
-    ];
-
-    /**
-     * Array of attributes to setter functions (for deserialization of responses)
-     *
-     * @var array<string, string>
-     */
-    protected static array $setters = [
-        'tax_number' => 'setTaxNumber',
-        'sales_tax_identification_number' => 'setSalesTaxIdentificationNumber'
-    ];
-
-    /**
-     * Array of attributes to getter functions (for serialization of requests)
-     *
-     * @var array<string, string>
-     */
-    protected static array $getters = [
-        'tax_number' => 'getTaxNumber',
-        'sales_tax_identification_number' => 'getSalesTaxIdentificationNumber'
-    ];
-
-    /**
-     * Array of attributes where the key is the local name,
-     * and the value is the original name
-     *
-     * @return array<string, string>
-     */
-    public static function attributeMap(): array
-    {
-        return self::$attributeMap;
-    }
-
-    /**
-     * Array of attributes to setter functions (for deserialization of responses)
-     *
-     * @return array<string, string>
-     */
-    public static function setters(): array
-    {
-        return self::$setters;
-    }
-
-    /**
-     * Array of attributes to getter functions (for serialization of requests)
-     *
-     * @return array<string, string>
-     */
-    public static function getters(): array
-    {
-        return self::$getters;
-    }
-
-    /**
-     * The original name of the model.
-     *
-     * @return string
-     */
-    public function getModelName(): string
-    {
-        return self::$openAPIModelName;
-    }
-
-
-    /**
-     * Associative array for storing property values
-     *
-     * @var array
-     */
-    protected array $container = [];
-
-    /**
-     * Constructor
-     *
-     * @param array $data Associated array of property values initializing the model
-     */
-    public function __construct(?array $data = null)
-    {
-        $this->setIfExists('tax_number', $data ?? [], null);
-        $this->setIfExists('sales_tax_identification_number', $data ?? [], null);
-    }
-
-    /**
-    * Sets $this->container[$variableName] to the given data or to the given default Value; if $variableName
-    * is nullable and its value is set to null in the $fields array, then mark it as "set to null" in the
-    * $this->openAPINullablesSetToNull array
-    *
-    * @param string $variableName
-    * @param array  $fields
-    * @param mixed  $defaultValue
-    */
-    private function setIfExists(string $variableName, array $fields, mixed $defaultValue): void
-    {
-        if (self::isNullable($variableName) && array_key_exists($variableName, $fields) && is_null($fields[$variableName])) {
-            $this->openAPINullablesSetToNull[] = $variableName;
+            $result[$k] = $this->makeFormSafe($v);
         }
 
-        $this->container[$variableName] = $fields[$variableName] ?? $defaultValue;
+        return $result;
     }
 
     /**
-     * Show all the invalid properties with reasons.
+     * Flattens a multi-level array of data and generates a single-level array
+     * compatible with formdata - a single-level array where the keys use bracket
+     * notation to signify nested data.
      *
-     * @return string[] invalid properties with reasons
+     * credit: https://github.com/FranBar1966/FlatPHP
      */
-    public function listInvalidProperties(): array
+    public static function flatten(array $source, string $start = ''): array
     {
-        $invalidProperties = [];
+        $opt = [
+            'prefix'          => '[',
+            'suffix'          => ']',
+            'suffix-end'      => true,
+            'prefix-list'     => '[',
+            'suffix-list'     => ']',
+            'suffix-list-end' => true,
+        ];
 
-        return $invalidProperties;
-    }
-
-    /**
-     * Validate all the properties in the model
-     * return true if all passed
-     *
-     * @return bool True if all properties are valid
-     */
-    public function valid(): bool
-    {
-        return count($this->listInvalidProperties()) === 0;
-    }
-
-
-    /**
-     * Gets tax_number
-     *
-     * @return string|null
-     */
-    public function getTaxNumber(): ?string
-    {
-        return $this->container['tax_number'];
-    }
-
-    /**
-     * Sets tax_number
-     *
-     * @param string|null $tax_number Tax number
-     *
-     * @return $this
-     */
-    public function setTaxNumber(?string $tax_number): static
-    {
-        if (is_null($tax_number)) {
-            throw new InvalidArgumentException('non-nullable tax_number cannot be null');
-        }
-        $this->container['tax_number'] = $tax_number;
-
-        return $this;
-    }
-
-    /**
-     * Gets sales_tax_identification_number
-     *
-     * @return string|null
-     */
-    public function getSalesTaxIdentificationNumber(): ?string
-    {
-        return $this->container['sales_tax_identification_number'];
-    }
-
-    /**
-     * Sets sales_tax_identification_number
-     *
-     * @param string|null $sales_tax_identification_number Sales tax identification number
-     *
-     * @return $this
-     */
-    public function setSalesTaxIdentificationNumber(?string $sales_tax_identification_number): static
-    {
-        if (is_null($sales_tax_identification_number)) {
-            throw new InvalidArgumentException('non-nullable sales_tax_identification_number cannot be null');
-        }
-        $this->container['sales_tax_identification_number'] = $sales_tax_identification_number;
-
-        return $this;
-    }
-    /**
-     * Returns true if offset exists. False otherwise.
-     *
-     * @param integer $offset Offset
-     *
-     * @return boolean
-     */
-    public function offsetExists(mixed $offset): bool
-    {
-        return isset($this->container[$offset]);
-    }
-
-    /**
-     * Gets offset.
-     *
-     * @param integer $offset Offset
-     *
-     * @return mixed|null
-     */
-    #[ReturnTypeWillChange]
-    public function offsetGet(mixed $offset): mixed
-    {
-        return $this->container[$offset] ?? null;
-    }
-
-    /**
-     * Sets value based on offset.
-     *
-     * @param int|null $offset Offset
-     * @param mixed    $value  Value to be set
-     *
-     * @return void
-     */
-    public function offsetSet(mixed $offset, mixed $value): void
-    {
-        if (is_null($offset)) {
-            $this->container[] = $value;
+        if ($start === '') {
+            $currentPrefix = '';
+            $currentSuffix = '';
+            $currentSuffixEnd = false;
+        } elseif (array_is_list($source)) {
+            $currentPrefix = $opt['prefix-list'];
+            $currentSuffix = $opt['suffix-list'];
+            $currentSuffixEnd = $opt['suffix-list-end'];
         } else {
-            $this->container[$offset] = $value;
+            $currentPrefix = $opt['prefix'];
+            $currentSuffix = $opt['suffix'];
+            $currentSuffixEnd = $opt['suffix-end'];
         }
+
+        $currentName = $start;
+        $result = [];
+
+        foreach ($source as $key => $val) {
+            $currentName .= $currentPrefix . $key;
+
+            if (is_array($val) && !empty($val)) {
+                $currentName .= $currentSuffix;
+                $result += self::flatten($val, $currentName);
+            } else {
+                if ($currentSuffixEnd) {
+                    $currentName .= $currentSuffix;
+                }
+
+                $result[$currentName] = ObjectSerializer::toString($val);
+            }
+
+            $currentName = $start;
+        }
+
+        return $result;
     }
 
     /**
-     * Unsets offset.
+     * formdata must be limited to scalars or arrays of scalar values,
+     * or a resource for a file upload. Here we iterate through all available
+     * data and identify how to handle each scenario
      *
-     * @param integer $offset Offset
-     *
-     * @return void
+     * @param string|bool|array|DateTime|ArrayAccess|SplFileObject $value
      */
-    public function offsetUnset(mixed $offset): void
+    protected function makeFormSafe(mixed $value)
     {
-        unset($this->container[$offset]);
+        if ($value instanceof SplFileObject) {
+            return $this->processFiles([$value])[0];
+        }
+
+        if (is_resource($value)) {
+            $this->has_file = true;
+
+            return $value;
+        }
+
+        if ($value instanceof ModelInterface) {
+            return $this->processModel($value);
+        }
+
+        if (is_array($value) || is_object($value)) {
+            $data = [];
+
+            foreach ($value as $k => $v) {
+                $data[$k] = $this->makeFormSafe($v);
+            }
+
+            return $data;
+        }
+
+        return ObjectSerializer::toString($value);
     }
 
     /**
-     * Serializes the object to a value that can be serialized natively by json_encode().
-     * @link https://www.php.net/manual/en/jsonserializable.jsonserialize.php
-     *
-     * @return mixed Returns data which can be serialized by json_encode(), which is a value
-     * of any type other than a resource.
+     * We are able to handle nested ModelInterface. We do not simply call
+     * json_decode(json_encode()) because any given model may have binary data
+     * or other data that cannot be serialized to a JSON string
      */
-    #[ReturnTypeWillChange]
-    public function jsonSerialize(): mixed
+    protected function processModel(ModelInterface $model): array
     {
-        return ObjectSerializer::sanitizeForSerialization($this);
+        $result = [];
+
+        foreach ($model::openAPITypes() as $name => $type) {
+            $value = $model->offsetGet($name);
+
+            if ($value === null) {
+                continue;
+            }
+
+            if (str_contains($type, '\SplFileObject')) {
+                $file = is_array($value) ? $value : [$value];
+                $result[$name] = $this->processFiles($file);
+
+                continue;
+            }
+
+            if ($value instanceof ModelInterface) {
+                $result[$name] = $this->processModel($value);
+
+                continue;
+            }
+
+            if (is_array($value) || is_object($value)) {
+                $result[$name] = $this->makeFormSafe($value);
+
+                continue;
+            }
+
+            $result[$name] = ObjectSerializer::toString($value);
+        }
+
+        return $result;
     }
 
     /**
-     * Gets the string presentation of the object
-     *
-     * @return string
+     * Handle file data
      */
-    public function __toString(): string
+    protected function processFiles(array $files): array
     {
-        return json_encode(
-            ObjectSerializer::sanitizeForSerialization($this),
-            JSON_PRETTY_PRINT
-        );
+        $this->has_file = true;
+
+        $result = [];
+
+        foreach ($files as $i => $file) {
+            if (is_array($file)) {
+                $result[$i] = $this->processFiles($file);
+
+                continue;
+            }
+
+            if ($file instanceof StreamInterface) {
+                $result[$i] = $file;
+
+                continue;
+            }
+
+            if ($file instanceof SplFileObject) {
+                $result[$i] = $this->tryFopen($file);
+            }
+        }
+
+        return $result;
     }
 
-    /**
-     * Gets a header-safe presentation of the object
-     *
-     * @return string
-     */
-    public function toHeaderValue(): string
+    private function tryFopen(SplFileObject $file)
     {
-        return json_encode(ObjectSerializer::sanitizeForSerialization($this));
+        return Utils::tryFopen($file->getRealPath(), 'rb');
     }
 }
-
-
